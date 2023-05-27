a5219e67436d15892a81d610b9256228178767ce
karayamanemre <107217989+karayamanemre@users.noreply.github.com>
2023-05-27 21:46:05 +0300

    user register & encryption

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -15,6 +15,16 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
+// Middleware to hash password before saving user to database
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(12);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 const User = mongoose.model('User', userSchema);
 
 export default User;
---
+++ backend/utils/generateToken.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken';
+
+const generateToken = (res, userId) => {
+  const token = jwt.sign(
+    {
+      userId,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: '10d',
+    },
+  );
+
+  // Set JWT as HttpOnly cookie
+  res.cookie('jwt', token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== 'development',
+    sameSite: 'strict',
+    maxAge: 1000 * 60 * 60 * 24 * 10, // 10 days
+  });
+};
+
+export default generateToken;
