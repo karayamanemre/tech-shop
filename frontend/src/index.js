f43be65ae6100a878d44bafc0c22bb90b31c6d03
karayamanemre <107217989+karayamanemre@users.noreply.github.com>
2023-05-30 15:27:43 +0300

    payment screen created

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -17,6 +17,7 @@ import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentScreen';
 import PrivateRoute from './components/PrivateRoute';
 
 const router = createBrowserRouter(
@@ -30,6 +31,7 @@ const router = createBrowserRouter(
 
       <Route path="" element={<PrivateRoute />}>
         <Route path="/shipping" element={<ShippingScreen />} />
+        <Route path="/payment" element={<PaymentScreen />} />
       </Route>
     </Route>,
   ),
