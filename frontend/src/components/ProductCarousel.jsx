cc58d6e180935798e15f2bc0bb9335e2e66b2569
karayamanemre <107217989+karayamanemre@users.noreply.github.com>
2023-06-04 00:31:32 +0300

    Update ProductCarousel.jsx

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Carousel, Image } from 'react-bootstrap';
-import Loader from './Loader';
 import Message from './Message';
 import { useGetTopProductsQuery } from '../slices/productsApiSlice';
 
 const ProductCarousel = () => {
-  const { data: products, isLoading, error } = useGetTopProductsQuery();
+  const { data: products, error } = useGetTopProductsQuery();
 
-  return isLoading ? (
-    <Loader />
-  ) : error ? (
+  return error ? (
     <Message variant='danger'>{error?.data?.message || error.error}</Message>
   ) : (
     <Carousel pause='hover' className='bg-dark mb-4'>
